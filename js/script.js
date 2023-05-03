a4b1a4ada334b4af084076e5b0c52ebf42ba2d61
Pavlo6532 <lpn3005@gmail.com>
2023-05-03 15:57:16 +0300

    done home work

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let num1 = parseFloat(prompt("Enter first number:"));
+let num2 = parseFloat(prompt("Enter second number:"));
+let num3 = parseFloat(prompt("Enter third number:"));
+
+let mean = (num1 + num2 + num3) / 3;
+
+alert("The arithmetic mean of the three numbers is: " + mean);
