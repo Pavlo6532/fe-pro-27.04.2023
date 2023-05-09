06ba7a2c0b403211bdd0e155da75fa0677686e07
Pavlo6532 <lpn3005@gmail.com>
2023-05-09 19:52:10 +0300

    done hw4

--- js/script.js
+++ js/script.js
@@ -1,41 +1,59 @@
-let yearOfBirth = prompt("У якому році ви народились?");
-let city = prompt("У якому місті ви живете?");
-let favoriteSport = prompt("Який ваш улюблений вид спорту?");
+// 1
+for (let i = 20; i <= 30; i += 0.5) {
+  console.log(i);
+}
 
-if (yearOfBirth && city && favoriteSport) {
-  let age = new Date().getFullYear() - yearOfBirth;
-  let message = `Ваш вік: ${age}\n`;
+// 2
+for (let i = 1; i <= 10; i++) {
+  console.log(i * 10 + " dollars = " + i * 10 * 27 + " hrn");
+}
 
-  if (city.toLowerCase() === "київ") {
-    message += "Ти живеш у столиці України.";
-  } else if (city.toLowerCase() === "лондон") {
-    message += "Ти живеш у столиці Великої Британії.";
-  } else if (city.toLowerCase() === "вашингтон") {
-    message += "Ти живеш у столиці США.";
-  } else {
-    message += `Ти живеш у місті ${city}.`;
+// 3
+let n = 50;
+for (let i = 1; i <= 100; i++) {
+  if (i * i <= n) {
+    console.log(i);
   }
+}
 
-  if (favoriteSport.toLowerCase() === "футбол") {
-    message += "\nКруто! Хочеш стати Ліонелом Мессі?";
-  } else if (favoriteSport.toLowerCase() === "теніс") {
-    message += "\nКруто! Хочеш стати Рафаелем Надалем?";
-  } else if (favoriteSport.toLowerCase() === "баскетбол") {
-    message += "\nКруто! Хочеш стати Леброном Джеймсом?";
-  }
+// 4
+let n = 23;
+let isPrime = true;
 
-  alert(message);
+if (n <= 1) {
+  isPrime = false;
 } else {
-  let missingFields = [];
-  if (!yearOfBirth) {
-    missingFields.push("дату народження");
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      isPrime = false;
+      break;
+    }
   }
-  if (!city) {
-    missingFields.push("місто");
-  }
-  if (!favoriteSport) {
-    missingFields.push("вид спорту");
-  }
+}
 
-  alert(`Шкода, що Ви не захотіли ввести свій(ю) ${missingFields.join(", ")}!`);
+if (isPrime) {
+  console.log(n + " is prime.");
+} else {
+  console.log(n + " is not prime.");
+}
+
+// 5
+const n = 9;
+let canGetByPowerOf3 = false;
+
+for (let i = 0; i <= n; i++) {
+  let power = Math.pow(3, i);
+  if (power === n) {
+    canGetByPowerOf3 = true;
+    break;
+  }
+  if (power > n) {
+    break;
+  }
+}
+
+if (canGetByPowerOf3) {
+  console.log(`${n} can get by power Of 3`);
+} else {
+  console.log(`${n} can't get by power Of 3`);
 }
