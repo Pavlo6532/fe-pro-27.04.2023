06efb5f6ec9f9fa9e76d150f2fc4ffc7298769a5
Pavlo6532 <lpn3005@gmail.com>
2023-05-15 22:24:29 +0300

    done hw5

---
+++ script/script.js
@@ -0,0 +1,29 @@
+let length = parseInt(prompt("Enter the length of the array:"));
+let array = [];
+
+for (var i = 0; i < length; i++) {
+  let input = prompt("Enter an array element:");
+  array.push(input);
+}
+
+console.log("Initial array:", array);
+
+let numbers = array.filter(function (element) {
+  return !isNaN(parseFloat(element));
+});
+numbers.sort(function (a, b) {
+  return parseFloat(a) - parseFloat(b);
+});
+
+let strings = array.filter(function (element) {
+  return isNaN(parseFloat(element));
+});
+strings.sort();
+
+let sortedArray = numbers.concat(strings);
+
+console.log("Sorted array:", sortedArray);
+
+sortedArray.splice(1, 4);
+
+console.log("The array after removing elements 2 through 4:", sortedArray);
