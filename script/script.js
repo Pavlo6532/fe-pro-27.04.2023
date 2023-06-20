fd7002fe2a455910822a8677e2362b1e97c7fed2
Pavlo6532 <lpn3005@gmail.com>
2023-06-20 18:50:34 +0300

    done hw17

--- script/script.js
+++ script/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+$(document).ready(function () {
+  $(".modal").click(function () {
+    $("body").addClass("modal-open");
+    $(".modal-wrapper").addClass("active").animate({ left: "0" }, 500);
+  });
+
+  $(".modal-window .close, .modal-wrapper").click(function (event) {
+    if (this === event.target) {
+      $("body").removeClass("modal-open");
+      $(".modal-wrapper").animate({ left: "-100%" }, 500, function () {
+        $(this).removeClass("active");
+      });
+    }
+  });
+
+  function disableScroll() {
+    $("body").css("overflow", "hidden");
+  }
+
+  function enableScroll() {
+    $("body").css("overflow", "auto");
+  }
+});
