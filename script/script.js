d733183a6326d5570823dac4b91a7a218255df49
Pavlo6532 <lpn3005@gmail.com>
2023-07-13 14:13:10 +0300

    done with closing function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+function createIterator() {
+  let count = 0;
+
+  const iterator = {
+    next: function () {
+      count++;
+      if (count === 1) {
+        console.log("start");
+      }
+      if (count <= 3) {
+        return { value: count, done: false };
+      } else {
+        console.log("finish");
+        return { done: true };
+      }
+    },
+  };
+
+  return iterator;
+}
+
+const iterator = createIterator();
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().done);
